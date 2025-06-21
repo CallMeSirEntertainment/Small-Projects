61f8102be7c781e619fa704b94d6b2f30e5d5d31
CallMeSirEntertainment <callmesirentertainment@outlook.com>
2025-06-21 15:40:33 -0500

    Refactor Fibonacci calculator: remove unused imports, simplify file saving, and improve memory management

--- Fibonacci Calculator.py
+++ Fibonacci Calculator.py
@@ -1,18 +1,18 @@
 from functools import cache
-from random import randint
-import time
 from os import system, name
+import gc
 from sys import set_int_max_str_digits
 set_int_max_str_digits(0)
 steps = []
 save_steps = False
 @cache
 def fib(n: int) -> int:
+    gc.collect()
     if n <=1:
         return n
     else:
         return fib(n - 1) + fib(n - 2)
-number = int(input('What fibonacci number would you like to calculate?\nWarning: Large calculations use a bunch of memory and RAM and may stall your CPU.\n'))
+number = int(input('What fibonacci number would you like to calculate?\nWarning: Large calculations use a bunch of RAM and may stall your PC.\n'))
 system('cls' if name == 'nt' else 'clear')
 save_to_file = input('Would you like to save the result to a file? (y/n)\n').lower()
 if save_to_file == 'y':
@@ -25,10 +25,9 @@ for i in range(0,number + 1):
     if save_steps == 'y':
         steps.append(num)
 if save_to_file == 'y':
-    id = randint(0,99999)
-    with open(f'fibonacci-result (ID {id}).txt', 'w') as file:
+    with open('fibonacci-result.txt', 'w') as file:
         system('cls' if name == 'nt' else 'clear')
-        print(f"Saving result to fibonacci-result (ID {id}).txt...\nDo not close this program or open the file.")
+        print("Saving result to fibonacci-result.txt...\nDo not close this program or open the file.")
         if save_steps == 'y':
             file.write('The final result is at the bottom of the file.\n\n')
             for i in range(len(steps) - 1):
@@ -37,11 +36,8 @@ if save_to_file == 'y':
             file.write(str(f'Final result:\n{str(fib(number))}.'))
         else:
             file.write(str(f'Number {number} in the fibonacci sequence is:\n{str(fib(number))}.'))
-        time.sleep(1)
     system('cls' if name == 'nt' else 'clear')
     print(f'Result saved to fibonacci-result (ID {id}).txt.')
-    time.sleep(60)
 else:
     system('cls' if name == 'nt' else 'clear')
     print(f'Number {number} in the fibonacci sequence is:\n{fib(number)}.')
-    time.sleep(60)
